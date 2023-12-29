7da6188acd9a0da638eec752eb78060f1db7a058
Muzammil khan <khanmuzammil549@gmail.com>
2023-12-30 02:25:48 +0530

    refactor: updated from dev

--- src/app/modules/people/people.component.tsx
+++ src/app/modules/people/people.component.tsx
@@ -1,82 +1,35 @@
 import { Person } from "./model";
 import { usePeopleQuery } from "./query";
-
 import "./people.css";
-import React, { useEffect, useState } from "react";
+import { useMemo, useState } from "react";
 
-const ITEMS_PER_PAGE = 10
-const DEFAULT_PAGE = 1
-const ITEMS_PER_PAGE_OPTIONS = [10,15, 20, 25, 50, 100];
+const ITEMS_PER_PAGE = 10;
+const DEFAULT_PAGE = 1;
+const COMBOBOX_OPTIONS = [10, 15, 20];
 
 export function People() {
   const { data: people, loading, error } = usePeopleQuery();
-  const [sortData, setSortData] = useState<Person[] | undefined>([])
-  const [sortOrder, setSortOrder] = useState<"ascending" | "descending" | undefined>("ascending");
-  const [searchData, setSearchData] = useState("")
+  const [sortOrder, setSortOrder] = useState('ascending');
+  const [searchQuery, setSearchQuery] = useState<string>('');
   const [currentPage, setCurrentPage] = useState(DEFAULT_PAGE);
   const [itemsPerPage, setItemsPerPage] = useState(ITEMS_PER_PAGE);
 
-  useEffect(() => {
-    if (people) {
-      sortByName("ascending");
-    }
-  }, [people]);
-  
-  const sortByName = (order?: "ascending" | "descending") => {
-    const sortedData = people?.slice().sort((a, b) => {
-      if (order === "ascending") {
-        return a.name.localeCompare(b.name);
-      } else {
-        return b.name.localeCompare(a.name);
-      }
+  const sortedPeople = useMemo(() => {
+    if (!people) return [];
+
+    return [...people].sort((a, b) => {
+      return sortOrder === 'ascending' ? a.name.localeCompare(b.name) : b.name.localeCompare(a.name);
     });
+  }, [people, sortOrder]);
 
-    setSortData(sortedData);
-    setSortOrder(order);
-  };
+  const filteredPeople = useMemo(() => {
+    if (!people || searchQuery === '') return sortedPeople;
 
-  const resetToDefault = () => {
-    setSortData(people)
-    setItemsPerPage(10)
-  }
-
-  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const searchTerm = event.target.value.toLowerCase();
-    setSearchData(searchTerm);
-
-    const filteredData = people?.slice().filter((person) =>
-      person.name.toLowerCase().includes(searchTerm)
+    return sortedPeople.filter((person) =>
+      person.name.toLowerCase().includes(searchQuery.toLowerCase())
     );
-
-    sortByName(sortOrder);
-    setSortData(filteredData);
-  };
-
-  const calculateFirstIndex = () => (currentPage - 1) * itemsPerPage;
-  const calculateLastIndex = () => calculateFirstIndex() + itemsPerPage;
-  const totalPages = Math.ceil((people?.length || 0)/ ITEMS_PER_PAGE);
-  const slicedPeople = sortData?.slice(calculateFirstIndex(), calculateLastIndex());
-
-  const goToFirstPage = () => {
-    setCurrentPage(DEFAULT_PAGE);
-  };
-
-  const goToLastPage = () => {
-    setCurrentPage(totalPages);
-  };
-
-  const goToNextPage = () => {
-    setCurrentPage((prevPage) => Math.min(prevPage + 1, totalPages));
-  };
-
-  const goToPreviousPage = () => {
-    setCurrentPage((prevPage) => Math.max(prevPage - 1, DEFAULT_PAGE));
-  };
-
-  const handleItemsPerPageChange = (value: number) => {
-    setItemsPerPage(value);
-    setCurrentPage(DEFAULT_PAGE);
-}
+  }, [sortedPeople, searchQuery]);
+  
   const renderCells = ({ name, show, actor, movies, dob }: Person) => (
     <>
       <td>{name}</td>
@@ -91,6 +44,34 @@ export function People() {
     </>
   );
 
+  const toggleSort = () => {
+    const newSortOrder = sortOrder === 'ascending' ? 'descending' : 'ascending';
+    setSortOrder(newSortOrder);
+  };
+  
+  const handleFirstPage = () => {
+    setCurrentPage(DEFAULT_PAGE);
+  };
+
+  const handleLastPage = () => {
+    const totalPages = Math.ceil(filteredPeople.length / itemsPerPage);
+    setCurrentPage(totalPages);
+  };
+
+  const handlePageChange = (newPage: number) => {
+    setCurrentPage(newPage);
+  };
+
+  const handleItemsPerPageChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const newItemsPerPage = parseInt(e.target.value, 10);
+    setItemsPerPage(newItemsPerPage);
+    setCurrentPage(DEFAULT_PAGE);
+  };
+  
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const visiblePeople = filteredPeople.slice(startIndex, endIndex);
+ 
   if (loading) {
     return <p>Fetching People...</p>;
   }
@@ -100,45 +81,63 @@ export function People() {
   }
 
   return (
-  <>
-    <div>
-      <div>
-        Filters
+    <>
+      <div className="filters-container">
+        <div>
+          <label htmlFor="Search">Search:</label>
+          <input
+            aria-label="Search"
+            type="text"
+            id="search"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
         </div>
-        <div className="sort-btn-container">
-          <button className="sort-btn" onClick={() => sortOrder === 'ascending' ? sortByName('descending') : sortByName('ascending')} aria-sort={sortOrder}>
-            {sortOrder === "ascending" ? "z to a" : "a to z"}
+        <div className="buttons-wrapper">
+          <button
+            onClick={() => handleFirstPage()}
+            disabled={currentPage === DEFAULT_PAGE}
+          >
+            First
           </button>
-          <button className="sort-btn" onClick={() => resetToDefault()}>Reset to Default</button>
-
-          <button aria-labelledby="First" onClick={() => goToFirstPage()} disabled={currentPage === DEFAULT_PAGE}>First</button>
-          <button aria-labelledby="Last" onClick={() => goToLastPage()} disabled={currentPage === totalPages}>Last</button>
-          <button aria-labelledby="Previous" onClick={() => goToPreviousPage()} disabled={currentPage === DEFAULT_PAGE}>Previous</button>
-          <button aria-labelledby="Next" onClick={() => goToNextPage()} disabled={currentPage === totalPages}>Next</button>
-
-          <label htmlFor="Search">
-            <input type="text" aria-labelledby="Search" id="Search" placeholder={"enter name"} value={searchData} onChange={e => handleSearch(e)}/>
-          </label>
-
-          <label htmlFor="ItemsPerPage">
-            Items per page:
-            <select id="ItemsPerPage" value={itemsPerPage} onChange={(e) => handleItemsPerPageChange(Number(e.target.value))}>
-              {ITEMS_PER_PAGE_OPTIONS.map((option) => (
-                <option key={option} value={option}>
-                  {option}
-                </option>
-              ))}
-            </select>
-          </label>
-
-          {people && <div>{`Showing ${calculateFirstIndex() + 1} - ${calculateLastIndex()} of ${people.length}`}</div>}
+          <button
+            onClick={() => handlePageChange(currentPage - 1)}
+            disabled={currentPage === DEFAULT_PAGE}
+          >
+            Previous
+          </button>
+          <button
+            onClick={() => handlePageChange(currentPage + 1)}
+            disabled={endIndex >= filteredPeople.length}
+          >
+            Next
+          </button>
+          <button
+            onClick={() => handleLastPage()}
+            disabled={endIndex >= filteredPeople.length}
+          >
+            Last
+          </button>
         </div>
-    </div>
-
+          {visiblePeople && <p className="result">Showing {startIndex + 1}-{endIndex} of {filteredPeople.length} </p> }
+          <div>
+          <label htmlFor="selectOptions">Items per page:</label>
+          <select aria-label="selectOptions" id="selectOptions"
+           onChange={handleItemsPerPageChange}
+           value={itemsPerPage}
+          >
+          {COMBOBOX_OPTIONS.map((option) => (
+              <option aria-label="option" key={option} value={option}>
+                {option}
+              </option>
+            ))}
+          </select>
+          </div>
+      </div>
     <table>
       <thead>
         <tr>
-          <th onClick={() => sortOrder === 'ascending' ? sortByName('descending') : sortByName('ascending')} aria-sort={sortOrder}>Name</th>
+          <th onClick={() => toggleSort()} aria-sort={sortOrder === 'ascending' ? 'ascending' : 'descending'}>Name</th>
           <th>Show</th>
           <th>Actor/Actress</th>
           <th>Date of birth</th>
@@ -147,14 +146,15 @@ export function People() {
       </thead>
 
       <tbody>
-        {slicedPeople && slicedPeople.length > 0 ? slicedPeople.map((people, index) => (
-          <tr key={index}>{renderCells(people)}</tr>
-        ))
-          :
+        {visiblePeople && visiblePeople.length > 0 ? (
+          visiblePeople.map((person, index) => (
+            <tr key={index}>{renderCells(person)}</tr>
+          ))
+        ) : (
           <tr>
             <td colSpan={5}>No People Available.</td>
           </tr>
-        }
+        )}
       </tbody>
     </table>
     </>
